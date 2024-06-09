144087a7c38f36134010094d6abfbc5933cae26a
bvr2703 <bharathvr2703@gmail.com>
2024-06-09 10:07:34 -0400

    added 3 ML models code

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,13 @@
 
 
 print("ML code written here!")
+
+
+def model1():
+    print("Model1 code added here")
+
+def model2():
+    print("Model2 code added here")
+
+def model3():
+    print("Model3 code added here")
